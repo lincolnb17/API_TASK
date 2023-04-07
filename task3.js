ccd8748c20426d722951a48ad05771d69f444dba
Lincoln Bhattachan <bhattachanl17@gmail.com>
2023-04-08 00:30:50 +0545

    done

--- task3.js
+++ task3.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 const url = 'https://reqres.in/api/users?page=2'
+const url2= 'https://reqres.in/api/users'
 
 // // send the GET request
 const response = await fetch(url,{
@@ -16,10 +17,24 @@ const modifiedData = {
     job: 'Docotor'
   };
 const parsedData = JSON.stringify(modifiedData,null,2);
-console.log(parsedData)
 
 // Writing the response to JSON File
 fs.writeFile('payload/data3.json', parsedData, (err) => {
   if (err) throw err;
   console.log('Data written to file');
 });
+
+
+
+
+// Read the JSON file and sending post request
+const data2= fs.readFileSync("./payload/data3.json", "utf-8");
+
+// // send the POST request
+const response2 = await fetch(url2,{
+  method:"POST",
+  body:data2,
+  headers:{"Content-Type":"application/json"}
+})
+const jsonData2 = await response2.json();
+console.log(jsonData2)
